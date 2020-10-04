fe958de7a99457a50c51b7651d319a2daffa6e90
Leila Córdova <leilacordova6@gmail.com>
2020-10-04 17:18:52 -0500

    probando

--- app/Views/QuienesSomos.php
+++ app/Views/QuienesSomos.php
@@ -20,6 +20,7 @@
     <!-- Custom styles for this template -->
     <link href="carousel.css" rel="stylesheet">
     <link href="<?php echo base_url();?>/resources/css/estilo.css" rel="stylesheet">
+    <link href="<?php echo base_url();?>/resources/css/estilocarrusel.css" rel="stylesheet">
 
     
   </head>
@@ -174,7 +175,7 @@
               <div class="card">
                 <div class="card-body">
                   <h5 class="card-title">MISIÓN</h5>
-                  <p class="card-text">Consiste en apoyar a los usuarios con el uso de las herramientas digitales. </p>
+                  <p class="card-text">La aplicación web consiste en apoyar a los usuarios con el uso de las herramientas digitales para desarrollar sus actividades personales o laborales. </p>
                 </div>
               </div>
             </div>
@@ -190,6 +191,48 @@
         </div>
       </div>
 
+      <div class="container-xl">
+        <div class="row">
+          <div class="col-lg-8 mx-auto">
+            <h2>Testimonials</h2>
+            <div id="myCarousel" class="carousel slide" data-ride="carousel">
+              <!-- Carousel indicators -->
+              <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+                <li data-target="#myCarousel" data-slide-to="2"></li>
+              </ol>   
+              <!-- Wrapper for carousel items -->
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <div class="img-box"><img src="/examples/images/clients/1.jpg" alt=""></div>
+                  <p class="testimonial">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu sem tempor, varius quam at, luctus dui. Mauris magna metus, dapibus nec turpis vel, semper malesuada ante. Idac bibendum scelerisque non non purus. Suspendisse varius nibh non aliquet.</p>
+                  <p class="overview"><b>Paula Wilson</b>, Media Analyst</p>
+                </div>
+                <div class="carousel-item">
+                  <div class="img-box"><img src="/examples/images/clients/2.jpg" alt=""></div>
+                  <p class="testimonial">Vestibulum quis quam ut magna consequat faucibus. Pellentesque eget nisi a mi suscipit tincidunt. Utmtc tempus dictum risus. Pellentesque viverra sagittis quam at mattis. Suspendisse potenti. Aliquam sit amet gravida nibh, facilisis gravida odio.</p>
+                  <p class="overview"><b>Antonio Moreno</b>, Web Developer</p>
+                </div>
+                <div class="carousel-item">
+                  <div class="img-box"><img src="/examples/images/clients/3.jpg" alt=""></div>
+                  <p class="testimonial">Phasellus vitae suscipit justo. Mauris pharetra feugiat ante id lacinia. Etiam faucibus mauris id tempor egestas. Duis luctus turpis at accumsan tincidunt. Phasellus risus risus, volutpat vel tellus ac, tincidunt fringilla massa. Etiam hendrerit dolor eget rutrum.</p>
+                  <p class="overview"><b>Michael Holz</b>, Seo Analyst</p>
+                </div>
+              </div>
+              <!-- Carousel controls -->
+              <a class="carousel-control-prev" href="#myCarousel" data-slide="prev">
+                <i class="fa fa-angle-left"></i>
+              </a>
+              <a class="carousel-control-next" href="#myCarousel" data-slide="next">
+                <i class="fa fa-angle-right"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <br></br>
 
       <footer>        
         <p class="float-right tex"><a href="#">Back to top</a></p>
